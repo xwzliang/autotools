27d589a382d1f628b55eee649e448a524a00c988
broliang <18706703437@163.com>
2020-01-25 18:38:38 +0800

    Add Makefile.am, Delete Makefile.in and autogen.sh: Use Automake for automatic makefiles.

--- autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# The --install option supported by autoreconf is designed to pass tool-specific options down to each of the tools that it calls in order to install missing files.
-autoreconf --install
-# Use automake to copy the missed install-sh
-# --copy option indicates that true copies should be made--otherwise, symbolic links are created to the files where the automake package has installed them
-automake --add-missing --copy >/dev/null 2>&1
