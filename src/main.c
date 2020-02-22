7e3b000df58f36b9915fbc8a4e5f7c38d37efcc1
broliang <18706703437@163.com>
2020-02-22 22:20:23 +0800

    Modify: Use Libtool's ltdl library.

--- src/main.c
+++ src/main.c
@@ -5,29 +5,36 @@
 # include <config.h>
 #endif
 
-#if HAVE_DLFCN_H
-# include <dlfcn.h>
+#if HAVE_LTDL_H
+# include <ltdl.h>
 #endif
 
 #define DEFAULT_SALUTATION "Hello"
 
 int main(int argc, char * argv[]) {
 	const char * salutation = DEFAULT_SALUTATION;
-#if HAVE_DLFCN_H
-	void * module;
+#if HAVE_LTDL_H
+	int ltdl;
+	lt_dlhandle module;
 	get_salutation_t * get_salutation_fp = 0;
 
-	module = dlopen("./module.so", RTLD_NOW);
-	if (module != 0) {
-		get_salutation_fp = (get_salutation_t *) dlsym(module, GET_SALUTATION_SYM);
-		if (get_salutation_fp != 0)
-			salutation = get_salutation_fp();
+	LTDL_SET_PRELOADED_SYMBOLS();
+
+	ltdl = lt_dlinit();
+	if (ltdl == 0) {
+		module = lt_dlopen("modules/hithere/hithere.la");
+		if (module != 0) {
+			get_salutation_fp = (get_salutation_t *) lt_dlsym(module, GET_SALUTATION_SYM);
+			if (get_salutation_fp != 0)
+				salutation = get_salutation_fp();
+		}
 	}
 #endif
 	jupiter_print(salutation, argv[0]);
-#if HAVE_DLFCN_H
+#if HAVE_LTDL_H
 	if (module != 0)
-		dlclose(module);
+		lt_dlclose(module);
+	lt_dlexit();
 #endif
 	return 0;
 }
